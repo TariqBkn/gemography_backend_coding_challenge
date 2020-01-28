a2134e0dab6140089c9dc6e1980b3538b84bf1d6
bkn <boukouyen.tariq@gmail.com>
2020-01-28 18:00:41 +0100

    Exclude data Sources from Spring Boot AutoConfig(No DB is used)

--- src/main/java/com/gitrepos/trends/TrendsApplication.java
+++ src/main/java/com/gitrepos/trends/TrendsApplication.java
@@ -1,12 +1,16 @@
 package com.gitrepos.trends;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @ComponentScan(value = {"com.gitrepos.trends","com.gitrepos.trends.modelImpl","com.gitrepos.trends.modelImpl"})
+// disabling the auto configuration for the a datasource because no DB is used
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class TrendsApplication {
 
 	public static void main(String[] args) {
